c2a95cc55d808d63d5f522a88239c386c8b9da34
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-02-28 13:20:17 +0500

    add more inout button where the value is in by user and submit i use prevent default to prevent default action

--- React06/src/assets/Form.jsx
+++ React06/src/assets/Form.jsx
@@ -1,21 +1,44 @@
 import { useState } from "react"
 
 export default function Form(){
-let [fullName,SetfullName] =useState("Awais");
+let [formData, setFormData] = useState({ fullName: "", userName: "" });
     return(
-        <div className="flex flex-col items-center mt-12">
+        <div >
+            <div className="flex flex-col items-center mt-12">
+            <label htmlFor="fullName">
             <input 
                 type="text" 
-                placeholder="Write something" 
-                value={fullName} 
-                onChange={(e)=>SetfullName(e.target.value) }
+                placeholder="Write Name" 
+                value={formData.fullName} 
+                id="fullName"
+                onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                 className="p-2 w-72 mb-5 rounded border border-gray-300"
             />
             <button 
-                className="px-4 py-2 bg-blue-500 text-white border-none rounded cursor-pointer"
+                className="px-4 py-2 bg-blue-500 text-white border-none rounded cursor-pointer"onSubmit={(e)=>e.preventDefault()}
             >
                 Submit
             </button>
+            </label>
+            </div>
+            <div className="flex flex-col items-center mt-12">
+            <label htmlFor="userName">
+            <input 
+                type="text" 
+                placeholder="Write user Name" 
+                value={formData.userName} 
+                id="userName"
+                onChange={(e) => setFormData({ ...formData, userName: e.target.value })}
+                className="p-2 w-72 mb-5 rounded border border-gray-300"
+            />
+            <button 
+                className="px-4 py-2 bg-blue-500 text-white border-none rounded cursor-pointer" onSubmit={(e)=>e.preventDefault()}
+            >
+                Submit
+            </button>
+            </label>
+            </div>
         </div>
+        
     )
 }
